c9c30762a2567eea8c8a4acc1cae79ae540d2818
Lotie Pham <150778069+lotie-pham@users.noreply.github.com>
2024-10-09 10:05:55 +0700

    Update index.js

--- index.js
+++ index.js
@@ -8,23 +8,29 @@ const STATUS_CODES = {
     UNAUTHORIZED: 401,
     INTERNAL_SERVER_ERROR: 500,
 };
-    fs.readFile('OneDrive_1_10-1-2024/9.1.png', async (err, data) => {
-    // fs.readFile('OneDrive_1_9-27-2024/invalid/31.2.png', async (err, data) => {
+    // fs.readFile('OneDrive_1_10-1-2024/9.1.png', async (err, data) => {
+    // fs.readFile('OneDrive_1_10-1-2024/22.9.1.jfif', async (err, data) => {
+    // fs.readFile('OneDrive_1_10-1-2024/24.1.jfif', async (err, data) => {
+    // fs.readFile('OneDrive_1_9-27-2024/invalid/25.2.png', async (err, data) => {
+    // fs.readFile('test/6.1.png', async (err, data) => {
+    // fs.readFile('test/15.1.png', async (err, data) => {
+    fs.readFile('test/50.1.jfif', async (err, data) => {
     if (err) throw err;
     const encodedImage = data.toString('base64');
 
-    // fs.writeFile('output.txt', encodedImage, (err) => {
-    //     if (err) {
-    //         console.error('Error writing to file:', err);
-    //     } else {
-    //         console.log('Base64 encoded image has been saved to output.txt');
-    //     }
-    // });
+    fs.writeFile('output.txt', encodedImage, (err) => {
+        if (err) {
+            console.error('Error writing to file:', err);
+        } else {
+            console.log('Base64 encoded image has been saved to output.txt');
+        }
+    });
 
 
     try {
-        const url = 'https://btr2i3e0gj.execute-api.us-west-2.amazonaws.com/test';  // Đường dẫn đến API của bạn
+        const url = '';  // Đường dẫn đến API của bạn
         const data = {
+        // const data = {
             encodedImages: encodedImage
         };
 
@@ -33,14 +39,15 @@ const STATUS_CODES = {
                 'Content-Type': 'application/json'
             }
         });
-        let statusCode = response.data.statusCode
+        console.log(response.data)
+        // let statusCode = response.data.statusCode
 
-        if (statusCode == STATUS_CODES.SUCCESS) {
-            console.log(JSON.parse(response.data.body));
-        }
-        else if (statusCode == STATUS_CODES.UNDEFINED || statusCode == STATUS_CODES.BAD_REQUEST) {
-            console.log(response.data)
-        }
+        // if (statusCode == STATUS_CODES.SUCCESS) {
+        //     console.log(JSON.parse(response.data.body));
+        // }
+        // else if (statusCode == STATUS_CODES.UNDEFINED || statusCode == STATUS_CODES.BAD_REQUEST) {
+        //     console.log(response.data)
+        // }
 
     } catch (error) {
         console.error('Error:', error.message);
